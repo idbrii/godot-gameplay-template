5705a84189cb258fdca4649f626e31d014cf4b01
David Briscoe <idbrii@gmail.com>
2024-07-14 23:12:25 -0700

    Upgrade to godot 4

--- addons/toggle-external-editor/plugin.gd
+++ addons/toggle-external-editor/plugin.gd
@@ -1,4 +1,4 @@
-tool
+@tool
 extends EditorPlugin
 # Source: https://github.com/godotengine/godot-proposals/issues/3659#issuecomment-1766638481
 
@@ -6,9 +6,9 @@ var checkbox = CheckBox.new()
 var settings = get_editor_interface().get_editor_settings()
 
 func _enter_tree():
-    checkbox.text = "External Editor"
+    checkbox.text = "Use External Editor"
     checkbox.set_pressed(settings.get("text_editor/external/use_external_editor"))
-    checkbox.connect("toggled", self, "_on_CheckBox_toggled")
+    checkbox.toggled.connect(_on_CheckBox_toggled)
     add_control_to_container(EditorPlugin.CONTAINER_TOOLBAR, checkbox)
 
 func _exit_tree():
