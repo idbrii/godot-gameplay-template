a605a628c39845ccf9e792779973ccbd8ee2269d
David Briscoe <idbrii@gmail.com>
2024-12-23 11:40:37 -0800

    Cleanup: gdformat toggle-external-editor

--- addons/toggle-external-editor/plugin.gd
+++ addons/toggle-external-editor/plugin.gd
@@ -5,15 +5,18 @@ extends EditorPlugin
 var checkbox = CheckBox.new()
 var settings = get_editor_interface().get_editor_settings()
 
+
 func _enter_tree():
     checkbox.text = "Use External Editor"
     checkbox.set_pressed(settings.get("text_editor/external/use_external_editor"))
     checkbox.toggled.connect(_on_CheckBox_toggled)
     add_control_to_container(EditorPlugin.CONTAINER_TOOLBAR, checkbox)
 
+
 func _exit_tree():
     remove_control_from_container(EditorPlugin.CONTAINER_TOOLBAR, checkbox)
     checkbox.free()
+
 
 func _on_CheckBox_toggled(pressed):
     settings.set("text_editor/external/use_external_editor", pressed)
